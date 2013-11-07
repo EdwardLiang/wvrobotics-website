9451ae25fc5d0e9b8d1e8f70547ebb5a2ae6afe7
Edward Liang <edward.k.liang@gmail.com>
2013-11-07 15:58:31 -0800

    Files added.

---
+++ includes/header.php
@@ -0,0 +1,26 @@
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<title>Westview Robotics</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="">
+<meta name="author" content="">
+<!-- Le styles -->
+<link href="assets/biscaya/theme/css/bootstrap.css" rel="stylesheet">
+<link href="assets/biscaya/theme/css/style.css" rel="stylesheet">
+<link href="assets/biscaya/theme/css/camera.css" rel="stylesheet">
+<link href="assets/biscaya/theme/css/icons.css" rel="stylesheet">
+<link href="assets/biscaya/theme/css/skin-yellow.css" rel="stylesheet">
+<link href="assets/biscaya/theme/css/bootstrap-responsive.css" rel="stylesheet">
+<!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+<!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+<!-- Fav and touch icons -->
+<link rel="apple-touch-icon-precomposed" sizes="144x144" href="assets/biscaya/theme/img/ico/apple-touch-icon-144-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="114x114" href="assets/biscaya/theme/img/ico/apple-touch-icon-114-precomposed.png">
+<link rel="apple-touch-icon-precomposed" sizes="72x72" href="assets/biscaya/theme/img/ico/apple-touch-icon-72-precomposed.png">
+<link rel="apple-touch-icon-precomposed" href="assets/biscaya/theme/img/ico/apple-touch-icon-57-precomposed.png">
+<link rel="shortcut icon" href="assets/biscaya/theme/img/ico/favicon.png">
+</head>
+
