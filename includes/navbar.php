84bab6d521a540031a384afeeed9833f6bfa517c
Edward Liang <edward.k.liang@gmail.com>
2013-11-07 23:22:51 -0800

    Navbar modifications.

--- includes/navbar.php
+++ includes/navbar.php
@@ -2,21 +2,12 @@
 <div class="container clearfix">
   <div class="row-fluid">
     <div class="span12">
-      <h1 class="brandlogo"><a href="index.html"><img src="assets/biscaya/theme/img/logo.png" alt=""></a></h1>
+      <h1 class="brandlogo"><a href="index.html"><img src="assets/img/header-robotics.png" alt=""></a></h1>
       <div class="topinfo">
         <ul class="social-icons list-soc">
-          <li><a href="#"><i class="icon-facebook"></i></a></li>
-          <li><a href="#"><i class="icon-twitter"></i></a></li>
-          <li><a href="#"><i class="icon-linkedin"></i></a></li>
-          <li><a href="#"><i class="icon-google-plus"></i></a></li>
-          <li><a href="#"><i class="icon-pinterest"></i></a></li>
+          <li><a href="https://www.facebook.com/FIRSTWVRobotics"><i class="icon-facebook"></i></a></li>
+          <li><a href="https://www.twitter.com/option_16"><i class="icon-twitter"></i></a></li>
         </ul>
-        <div class="infophone">
-          <i class="icon-phone"></i> Tel: +8 737 924 6035
-        </div>
-        <div class="infoaddress">
-           3953 Berkley Street, Fort Washington, 19034
-        </div>
       </div>
       <div class="clearfix">
       </div>
@@ -25,31 +16,24 @@
           <ul id="nav" class="nav">
             <li class="active selected"><a href="index.html">Home</a></li>
             <li class="divider-vertical"></li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Templates <b class="caret"></b></a>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">About Us <b class="caret"></b></a>
             <ul class="dropdown-menu js-activated" style="display: none;">
-              <li><a href="about.html">About Us</a></li>
-              <li><a href="services.html">Services</a></li>
-              <li><a href="faq.html">Knowledge Base</a></li>
-              <li><a href="icons.html">Icons</a></li>
-              <li><a href="testimonials.html">Testimonials</a></li>
-              <li><a href="404.html">404 Error</a></li>
+              <li><a href="about.php">About</a></li>
+              <!--.dropdown-->
+            </ul>
+            </li>
+                        <li class="divider-vertical"></li>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">FRC 3341 <b class="caret"></b></a>
+            <ul class="dropdown-menu js-activated" style="display: none;">
+              <li><a href="columns.html">Atlas</a></li>
+              <li><a href="masonry4.html">Peace Keeper</a></li>
+              <li><a href="masonry3.html">Fat Man</a></li>
+              <li><a href="masonry2.html">Trinity</a></li>
               <!--.dropdown-->
             </ul>
             </li>
             <li class="divider-vertical"></li>
-            <li><a href="elements.html">Elements</a></li>
-            <li class="divider-vertical"></li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Layouts <b class="caret"></b></a>
-            <ul class="dropdown-menu js-activated" style="display: none;">
-              <li><a href="columns.html">Columns</a></li>
-              <li><a href="masonry4.html">Masonry 4</a></li>
-              <li><a href="masonry3.html">Masonry 3</a></li>
-              <li><a href="masonry2.html">Masonry 2</a></li>
-              <!--.dropdown-->
-            </ul>
-            </li>
-            <li class="divider-vertical"></li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Portfolio <b class="caret"></b></a>
+            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">FTC 5555 <b class="caret"></b></a>
             <ul class="dropdown-menu js-activated" style="display: none;">
               <li><a href="portfolio.html">Portfolio List</a></li>
               <li><a href="projectdetail.html">Project Detail</a></li>
@@ -57,21 +41,14 @@
             </ul>
             </li>
             <li class="divider-vertical"></li>
-            <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown">Blog <b class="caret"></b></a>
-            <ul class="dropdown-menu js-activated" style="display: none;">
-              <li><a href="bloghome.html">Blog Home</a></li>
-              <li><a href="blogsingle.html">Single Post</a></li>
-              <!--.dropdown-->
-            </ul>
-            </li>
+            <li><a href="outreach.php">Outreach</a></li>
             <li class="divider-vertical"></li>
-            <li><a href="contact.html">Contact</a></li>
+            <li><a href="sponsors.php">Sponsors</a></li>
             <li class="divider-vertical"></li>
+            <li><a href="contact.php">Contact</a></li>
+            <li class="divider-vertical"></li>
+
           </ul>
-          <form id="search" action="#" method="GET">							
-              <input type="text" onfocus="if(this.value =='Enter search keywords here...' ) this.value=''" onblur="if(this.value=='') this.value='Enter search keywords here...'" value="Enter search keywords here..." name="s">
-              <a href="#"></a>							
-          </form>
         </div>
       </div>
     </div>
