2f37e38b6b680104a4d65c8b3eebd1f18959a944
Edward Liang <edward.k.liang@gmail.com>
2013-11-09 15:38:12 -0800

    Typo fix.

--- frc/trinity.php
+++ frc/trinity.php
@@ -7,7 +7,7 @@
 			<div class="span6">
 				<p>
 					<b>Year</b> : 2010 | <b>Challenge</b> : Breakaway | <b>Type</b> : FRC </p>
-          <p>In BREAKAWAY<sup>SM</sup>, two alliances of three teams each compete on a 27' by 54' field with bumps attempting to earn poitns by collecting soccer balls in their goals. Additional bonus points are earned for each robot not touching the field at the end of the match.
+          <p>In BREAKAWAY<sup>SM</sup>, two alliances of three teams each compete on a 27' by 54' field with bumps attempting to earn points by collecting soccer balls in their goals. Additional bonus points are earned for each robot not touching the field at the end of the match.
  <a href="http://www.usfirst.org/uploadedFiles/Robotics_Programs/FRC/FRC_Communications_Resource_Center/Communication_Assets/2010_Archive_Assets/2010Game.pdf" target="_blank">Read more about the game.&nbsp;</a></p>
           <p>--First Robotics</p>
 				  				<br/>
