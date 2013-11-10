61130a1c91735482f8aaa5abe4c9d9e7878f97cb
Edward Liang <edward.k.liang@gmail.com>
2013-11-09 20:22:41 -0800

    Sponsors Page.

--- sponsors.php
+++ sponsors.php
@@ -11,6 +11,31 @@
 	</div>
 	<!-- /end header -->
 	<div class="container">
+
+<div class="page-header">
+			<h1>Sponsor Us</h1>
+			<div class="headerdivider">
+			</div>
+		</div>
+<p>The FIRST Robotics Competition is a program that requires funding from multiple sources; the entrance fee to a competition is $5,000.  We raise this money mainly through sponsorships and grants from various companies, corporations, or individuals. Those who sponsor us have their name on the robot and on our T-shirts. This provides publicity for the company, as well as funding for the team. Sponsorships generally range from $2,000 to $5,000, but any amount is appreciated. If you would like to sponsor us, please send an email to <a href="mailto:wvrobotics@gmail.com">wvrobotics@gmail.com</a> with the subject as "Sponsorship". Thank you for your consideration, and we look forward to partnering with you!</p>
+<br>
+<br>
+ <div class="page-header">
+			<h1>Our Sponsors</h1>
+			<div class="headerdivider">
+			</div>
+		</div>
+
+<center>
+<a href="http://www.baesystems.com/" title="BAE Systems"><img src="assets/img/sponsors/bae_logo.png" alt="BAE Systems" width="320"></a>
+<a href="http://www.saic.com/" title="SAIC"><img src="assets/img/sponsors/SAIC_logo.png" alt="SAIC"></a>
+<a href="http://www.teradata.com/" title="Teradata"><img src="assets/img/sponsors/teradata_logo.png" width="200" alt="Teradata"></a>
+<a href="http://www.viasat.com/" title="Viasat"><img src="assets/img/sponsors/ViaSat Logo.jpg" alt="Viasat"></a>
+<a href="http://www.qualcomm.com/" title="Qualcomm"><img src="assets/img/sponsors/Qualcomm_logo.png" alt="Qualcomm"></a>
+<a href="http://www.carefusion.com/" title="Raytheon"><img src="assets/img/sponsors/CareFusion_2.png" alt="CareFusion"></a>
+<a href="http://www.raytheon.com/" title="Northrop Grumman"><img src="assets/img/sponsors/RTN_RGB_RED.jpg" alt="Raytheon"></a>
+</center>
+
 	</div>
 <?php include('includes/footer.php'); ?>
 
@@ -69,7 +94,7 @@ $(window).load(function(){
 		$(this).siblings("h3").removeClass("active");
 	});	
 </script>
-<!-- Call opacity on hover images from recent news-->
+<!-- Call opacity on hover assets/img/sponsors from recent news-->
 <script type="text/javascript">
 $(document).ready(function(){
     $("img.imgOpa").hover(function() {
