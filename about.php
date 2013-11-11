7adb43fec4c62fbfc9c1d0444742271b514ca3db
Edward Liang <edward.k.liang@gmail.com>
2013-11-10 22:04:44 -0800

    Closed sections by default.

--- about.php
+++ about.php
@@ -92,6 +92,7 @@ $(document).ready(function(){
 <script type="text/javascript"> 
 $(document).ready(function () {
 // ---- FAQs ---------------------------------------------------------------------------------------------------------------
+$('.faqs dd').hide(); // Hide all DDs inside .faqs
 $('.faqs dt').hover(function(){$(this).addClass('hover')},function(){$(this).removeClass('hover')}).click(function(){ // Add class "hover" on dt when hover
 $(this).next().slideToggle('normal'); // Toggle dd when the respective dt is clicked
 }); 
